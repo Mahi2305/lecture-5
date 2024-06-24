88ae967937870c6439d84479a2d9b43828887474
Mahi2305 <114794143+Mahi2305@users.noreply.github.com>
2024-06-24 13:10:15 +0530

    Add files via upload

---
+++ lecture 5 - conditional statements.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b,c,d;
+cout<<"Input four integers a,b,c and d:"<<endl;
+cin>>a;
+cin>>b;
+cin>>c;
+cin>>d;
+cout<<(a>b && b>c);
+cout<<(a>b || b>c);
+}
